bbd015fd602e236c80ac48416dd94e13ae9774f3
Sarthak Sharma <sarthaksharma25702@gmail.com>
2025-12-08 17:39:34 +0530

    Fix Home.jsx before merging shivanidev-v1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Navbar from '../components/Navbar';
 import HomeHeroTop from '../components/HomeHeroTop';
 import HomeHeroTwo from '../components/HomeHeroTwo';
 import HomeHeroThree from '../components/HomeHeroThree';
+import HomeHeroFour from '../components/HomeHeroFour';
 import QuestionHero from '../components/QuestionHero';
 import TourSlider from '../components/TourSlider';
 import InternationalSlider from '../components/InternationalSlider';
